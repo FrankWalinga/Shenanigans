fbae84a696d289ffadbf773f063ffcedca0525d6
Frank Walinga <fwalinga@gmail.com>
2015-08-13 18:22:25 +0200

    Update imageReplacementShenanigans.js
    added time-bomb functionality to image replacement.

--- Chrome-extensions/imageReplacementShenanigans.js
+++ Chrome-extensions/imageReplacementShenanigans.js
@@ -3,20 +3,23 @@ var GREEK_URLS = ['https://i.imgur.com/YhNQH5X.jpg',
                                  'https://i.imgur.com/htKiEDA.png',
                                  'https://i.imgur.com/kThUaFY.jpg',
                                 ];
+var currentDate = new Date();
 
-// Pick out a random image from our collection.
-function getRandomImage() {
-    return GREEK_URLS[Math.floor(Math.random() * GREEK_URLS.length)];
-}
-
-// Get all the images on a page.
-var images = document.getElementsByTagName("img");
-
-// Replace each image with a random one.
-for (var i = 0; i < images.length; i++) {
-    var image = images[i];
-    if (Math.random() < 0.1) {
-        image.src = getRandomImage();
-        console.log(image);
-    }
+if(currentDate.getTime() > 1441105869000){
+  // Pick out a random image from our collection.
+  function getRandomImage() {
+      return GREEK_URLS[Math.floor(Math.random() * GREEK_URLS.length)];
+  }
+  
+  // Get all the images on a page.
+  var images = document.getElementsByTagName("img");
+  
+  // Replace each image with a random one.
+  for (var i = 0; i < images.length; i++) {
+      var image = images[i];
+      if (Math.random() < 0.1) {
+          image.src = getRandomImage();
+          console.log(image);
+      }
+  }
 }
