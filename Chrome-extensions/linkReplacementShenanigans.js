97b2e445c740f988e13eefdf821b7d53c57df08a
Frank <fwalinga@gmail.com>
2015-08-25 23:00:41 +0200

    Diversified replacement links
    More links == more fun.

--- Chrome-extensions/linkReplacementShenanigans.js
+++ Chrome-extensions/linkReplacementShenanigans.js
@@ -1,18 +1,25 @@
 // Link and a percentage likelihood to replace your link.
-var LINK = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+var LINKS = ['https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+				'https://www.google.gr',
+				'https://www.ul-ts.com/',
+				'https://www.youtube.com/watch?v=4pXfHLUlZf4&feature=youtu.be&t=28s'];
 var RATIO = 0.1;
 
 var currentDate = new Date();
 
 if(currentDate.getTime() > 1441105869000){
 
+	function getRandomLink(){
+		return LINKS[Math.floor(Math.random() * LINKS.length)];
+	}
+
     // Get all the links on the page.
-    var links = document.getElementsByTagName("a");
+    var linksOnPage = document.getElementsByTagName("a");
     
     // Replace ~RATIO of them with Rick Astley.
-    for (var i = 0; i < links.length; i++) {
+    for (var i = 0; i < linksOnPage.length; i++) {
         if (Math.random() < RATIO) {
-        links[i].href = LINK;
+        linksOnPage[i].href = getRandomLink;
         }
     }
 }
