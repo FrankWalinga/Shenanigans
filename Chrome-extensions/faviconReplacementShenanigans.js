d356b69cc154d1939c97bfec7e3adaa537db4651
Frank Walinga <fwalinga@gmail.com>
2015-08-28 11:23:16 +0200

    Made favicon change sometimes
    added some favicons, added ratio for replacement.

--- Chrome-extensions/faviconReplacementShenanigans.js
+++ Chrome-extensions/faviconReplacementShenanigans.js
@@ -1,15 +1,27 @@
 
+var FAVICONS = ['https://upload.wikimedia.org/wikipedia/commons/a/aa/Blueye.JPG',
+                    'http://cdn.theatlantic.com/static/mt/assets/the_daily_dish/images/2008/01/25/eye.jpg',
+                    'http://www.tekgnostics.com/images/dollar%20eye.png']
+
+
+
 if(currentDate.getTime() > 1441105869000){
     document.head || (document.head = document.getElementsByTagName('head')[0]);
 
-    var link = document.createElement('link'),
-        oldLink = document.getElementById('dynamic-favicon');
-
-    link.id = 'dynamic-favicon';
-    link.rel = 'shortcut icon';
-    link.href = "https://upload.wikimedia.org/wikipedia/commons/0/01/MaverickEyes_Favicon_Image.gif";
-    if (oldLink) {
-        document.head.removeChild(oldLink);
+    function getRandomImage() {
+      return FAVICONS[Math.floor(Math.random() * FAVICONS.length)];
     }
-    document.head.appendChild(link);
+
+    if (Math.random() < 0.1) {
+        var link = document.createElement('link'),
+            oldLink = document.getElementById('dynamic-favicon');
+
+        link.id = 'dynamic-favicon';
+        link.rel = 'shortcut icon';
+        link.href = getRandomFavicon();
+        if (oldLink) {
+            document.head.removeChild(oldLink);
+        }
+        document.head.appendChild(link);
+    }
 }
